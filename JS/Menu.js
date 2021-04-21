936e3a8de8d31d6eac976dde9c7e74d025ef699a
Alejandro-va <prof.jahz@gmail.com>
2021-04-21 16:59:01 -0300

    primer commit

---
+++ JS/Menu.js
@@ -0,0 +1,5 @@
+let btnMenu = document.getElementById("btnmenu");
+let menu = document.getElementById("menu");
+btnMenu.addEventListener("click", function(){"use strict";
+menu.classList.toggle("mostrar");
+});
